b043906f79f8f47f19b76f7d1668fd211b53ddf7
adifdwimaulana <adifdwimaulana@gmail.com>
2021-03-31 01:59:40 +0700

    fetch data onchange

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import axios from 'axios'
+import { Row, Col, Form, FormGroup } from 'react-bootstrap'
 
 function App() {
+  const [word, setWord] = useState('')
+
+  const handleSearch = (e) => {
+    const value = e.target.value
+
+    setWord(value)
+    axios({
+      method: 'post',
+      url: 'http://localhost:8000/api/word/search',
+      data: {
+        input: value
+      }
+    })
+    .then((response) => {
+      if(response.status === 200){
+        if(response.data.status === 200){
+          console.log(response.data.result)
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form>
+          <Form.Group controlId="word">
+              <Form.Label>Search words</Form.Label>
+              <Form.Control type="text" placeholder="Word" onChange={handleSearch} value={word} autoComplete="off" required />
+          </Form.Group>
+        </Form>
+        
       </header>
     </div>
   );
